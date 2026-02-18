917bd0158ef3c5af27bac35da0f0d4621a6a13ae
sarcela <72840283+sarcela@users.noreply.github.com>
2026-02-17 22:48:13 -0500

    Initial Commit

---
+++ OpenPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenPad
+//
+//  Created by Cesar Fraire on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenPad/OpenPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPadApp.swift
+//  OpenPad
+//
+//  Created by Cesar Fraire on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
